ac28577e749e3616dea5632d22c23509acf867ee
NicholG90 <nick@gourlay.me>
2021-11-18 18:01:08 -0500

    updating icons and other improvements

--- src/App.js
+++ src/App.js
@@ -4,53 +4,6 @@ import React from 'react';
 import Navigation from './navigation';
 import Main from  './main';
 
-
-var burgerstyles = {
-    bmBurgerButton: {
-      position: 'fixed',
-      width: '36px',
-      height: '30px',
-      left: '36px',
-      top: '36px'
-    },
-    bmBurgerBars: {
-      background: '#f4a261'
-    },
-    bmBurgerBarsHover: {
-      background: '#a90000'
-    },
-    bmCrossButton: {
-      height: '24px',
-      width: '24px'
-    },
-    bmCross: {
-      background: '#bdc3c7'
-    },
-    bmMenuWrap: {
-      position: 'fixed',
-      height: '100%'
-    },
-    bmMenu: {
-      background: '#f4a261',
-      padding: '2.5em 1.5em 0',
-      fontSize: '1.15em',
-    },
-    bmMorphShape: {
-      fill: '#f4a261'
-    },
-    bmItemList: {
-      color: '#2a9d8f',
-      padding: '0.8em'
-    },
-    bmItem: {
-      display: 'inline-block'
-    },
-    bmOverlay: {
-      background: 'rgba(0, 0, 0, 0.3)'
-    }
-  }
-  
-
 class App extends React.Component {
   showSettings (event) {
     event.preventDefault();
@@ -60,9 +13,11 @@ class App extends React.Component {
   render () {
     return (
       <div id="outer-container">
-      <Menu styles={ burgerstyles } pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
+      <header id="burgerHeader">
+      <Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
         <Navigation />
       </Menu>
+      </header>
       <main id="page-wrap">
         <Main />
       </main>
